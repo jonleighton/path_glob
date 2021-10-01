c7959a812adcfa12f31562bbf69ab602ed772cc1
Jon Leighton <hello@jonleighton.name>
2021-10-01 15:44:05 +1000

    Generate mix project

---
+++ lib/path_glob.ex
@@ -0,0 +1,18 @@
+defmodule PathGlob do
+  @moduledoc """
+  Documentation for `PathGlob`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PathGlob.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/path_glob_test.exs
@@ -0,0 +1,8 @@
+defmodule PathGlobTest do
+  use ExUnit.Case
+  doctest PathGlob
+
+  test "greets the world" do
+    assert PathGlob.hello() == :world
+  end
+end
