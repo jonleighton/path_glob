5b615df64b00cf44286055461e5ab6faa0374954
Jon Leighton <hello@jonleighton.name>
2022-10-16 09:17:43 +1100

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PathGlob.MixProject do
   def project do
     [
       app: :path_glob,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: description(),
